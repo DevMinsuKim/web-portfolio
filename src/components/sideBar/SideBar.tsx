1e5482e86f6675ae5b1af86883d35b20fbf7a417
DevMinsuKim <cvnefr7704@gmail.com>
2023-11-21 00:28:14 +0900

    Feat: 사이드바 UI 수정 및 i18n 적용 중

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { sacramento } from "@/font/Font";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,6 +7,7 @@ import { FaCode } from "react-icons/fa6";
 import { FaLightbulb } from "react-icons/fa";
 import { HiOutlineOfficeBuilding } from "react-icons/hi";
 import { IoIosCall } from "react-icons/io";
+import { useState } from "react";
 
 export default function SideBar() {
   const menu = [
@@ -40,8 +42,17 @@ export default function SideBar() {
       icon: <IoIosCall className="h-5 w-5" />,
     },
   ];
+
+  const [language, setLanguage] = useState("한국어");
+
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) =>
+      prevLanguage === "한국어" ? "English" : "한국어",
+    );
+  };
+
   return (
-    <aside className="fixed left-0 top-0 h-screen min-w-[240px] max-w-[240px] overflow-y-auto rounded-r-3xl bg-gradient-to-r from-background to-background1">
+    <aside className="fixed left-0 top-0 flex h-screen min-w-[240px] max-w-[240px] flex-col overflow-y-auto rounded-r-3xl bg-gradient-to-r from-background to-background1">
       <header className="mt-12 flex min-h-[90px] items-center justify-center border-b border-gray pb-6">
         <Link href={""}>
           <Image
@@ -53,27 +64,34 @@ export default function SideBar() {
             height={100}
           />
           <h1
-            className={`${sacramento.className} animate-textGradient to-indigo via-sky bg-gradient-to-r from-primary bg-clip-text text-center text-5xl text-transparent`}
+            className={`${sacramento.className} animate-textGradient bg-gradient-to-r from-primary via-sky to-indigo bg-clip-text text-center text-5xl text-transparent`}
           >
             minsu
           </h1>
         </Link>
       </header>
-      <ul>
+      <ul className="mt-4 flex-grow">
         {menu.map(({ href, title, icon }) => (
           <Link key={href} href={href}>
             <li
               aria-label={title}
-              className="m-2 flex rounded-md p-2 duration-200 ease-in-out hover:bg-primary hover:text-white"
+              className="mx-3 flex items-center rounded-md px-3 py-3 duration-200 ease-in-out hover:bg-primary hover:text-white"
             >
-              <div className="flex h-8 w-8 items-center justify-center rounded-md">
-                {icon}
-              </div>
-              <p className="pl-4">{title}</p>
+              <div className="mr-4">{icon}</div>
+              {title}
             </li>
           </Link>
         ))}
       </ul>
+      <footer className="">
+        <div>
+          <button onClick={toggleLanguage}>
+            {language === "한국어" ? "Switch to English" : "한국어로 전환"}
+          </button>
+        </div>
+        <div>asdlfjsalkfdjsalkfjlk</div>
+        <div>sadfhjjksahdfjk</div>
+      </footer>
     </aside>
   );
 }
