c020a18b6bd54b5378d6b0f100647656006c809a
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-07-11 19:21:59 +0900

    refactor: next.js 14.2.5 초기 설정

--- src/app/[locale]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SideBar from "@/containers/sideBar/SideBar";
-
-export default function Home() {
-  return (
-    <main>
-      <SideBar />
-    </main>
-  );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        primary: "var(--color-primary)",
-        background: "var(--color-background)",
-        foreground: "var(--color-foreground)",
-        surface: "var(--color-surface)",
-      },
-      boxShadow: {
-        shadow: "2px 8px 30px 8px rgba(0,0,0,0.1)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
